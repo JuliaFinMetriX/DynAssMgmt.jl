fe9fff97aa81c56b4ab2e0833d30284f4fbe6e4a
cgroll <groll.christian.edu@gmail.com>
2017-09-18 16:17:28 +0200

    allow doc deploy

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,10 @@
 using Documenter, DynAssMgmt
 
 makedocs()
+
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo   = "github.com/cgroll/DynAssMgmt.git",
+    julia  = "0.5",
+    osname = "linux"
+)
