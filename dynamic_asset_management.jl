7fce6f4a68431dca506e928e8a00db29137bd8c6
cgroll <groll.christian.edu@gmail.com>
2017-11-06 15:11:50 +0100

    reduce dev code to true backtest

--- dynamic_asset_management.jl
+++ dynamic_asset_management.jl
@@ -1,3 +1,13 @@
+# TODO
+# - run backtests on large data
+# - allow universe subsets
+# - inspect moment estimators directly
+# - implement weight filters
+# - save and load portfolios
+# - check optimization status to catch errors
+# - identify failed strategies and inspect them
+
+
 ## set up parallel computation
 addprocs(3)
 
@@ -13,6 +23,7 @@ using Plots
 using DynAssMgmt
 using EconDatasets
 using TimeSeries
+using NamedArrays
 
 # load data
 xxRets = dataset("IndustryPfs")
@@ -21,8 +32,6 @@ xxRets = dataset("IndustryPfs")
 retType = ReturnType(true, false, Dates.Day(1), false)
 rets = Returns(xxRets, retType)
 
-Plots.gui()
-
 ## define efficient frontier / diversfication frontier strategies
 sigTargets = [linspace(.7, 1.4, 15)...]
 diversTarget = 0.9
@@ -30,7 +39,8 @@ divFrontStrat = DivFront(diversTarget, sigTargets)
 
 ## estimate historic moments
 ewmaEstimator = EWMA(0.99, 0.95)
-startInd = 22500
+# startInd = 500
+startInd = findfirst(rets.data.timestamp .>= Date(1950,1,1))
 @time univList = DynAssMgmt.applyOverTime(ewmaEstimator, rets, startInd)
 
 # get subsets
@@ -42,155 +52,52 @@ btUniverses = univList
 divFrontInvs = apply(divFrontStrat, btUniverses)
 equWgtsInvs = apply(DynAssMgmt.EqualWgts(), btUniverses)
 
-## inspect chosen portfolios
-xxInd = 10
-sigTarget = divFrontStrat.sigTargets[xxInd]
-stratName = "Sigma target $sigTarget"
-wgtsOverTime(divFrontInvs, xxInd, leg=false, title = stratName)
-
-# analyse diversification levels
-diversVals = pfDivers(divFrontInvs)
-stratLabels = [*("Sigma", " $sigTarget") for sigTarget in divFrontStrat.sigTargets]
-Plots.plot(divFrontInvs.dates, diversVals, labels = stratLabels)
-
-# show average diversification over time
-avgDiversification = mean(diversVals, 1)
-Plots.plot(stratLabels, avgDiversification[:], seriestype=:bar, leg=false,
-    xrotation = 45, xlabel = "Strategy", title = "Average diversification level", )
-
-# analyse sigma levels
-divFrontInvs
-nDays, nStrats, nAss = size(divFrontInvs)
-allCondSigs = zeros(nDays, nStrats)
-for ii=1:nDays
-    for jj=1:nStrats
-        mu, sig = pfMoments(btUniverses.universes[ii], divFrontInvs.pfs[ii, jj], "std")
-        allCondSigs[ii, jj] = sig
-    end
-end
-
-Plots.plot(divFrontInvs.dates, allCondSigs, labels = stratLabels)
-
-## analyse single peculiar universe
-dayToAnalyse = 50
-univToAnalyse = btUniverses.universes[dayToAnalyse]
-
-pfopts(univToAnalyse, doScale=true)
-
 ## evaluate performance
-perfs = DynAssMgmt.evalPerf(divFrontInvs, rets)
-equWgtsPerfs = DynAssMgmt.evalPerf(equWgtsInvs, rets)
+perfs = evalPerf(divFrontInvs, rets)
+equWgtsPerfs = evalPerf(equWgtsInvs, rets)
 
-# rename performance columns
-xx = rename(perfs.data, stratLabels)
-perfs = Performances(xx, perfs.retType)
+## impute NaNs
+oldPerfs = perfs
+xx = DynAssMgmt.locf(perfs.data.values)
+xxTA = TimeArray(perfs.data.timestamp, xx, perfs.data.colnames)
+perfs = Performances(xxTA, perfs.retType)
 
 # show performances
-Plots.plot(perfs, leg=:topleft)
-Plots.plot!(equWgtsPerfs.data.timestamp, equWgtsPerfs.data.values*100, lab="Equal weights", line = (2, :red))
+# Plots.gui()
+# Plots.plot(perfs, leg=:topleft)
+# Plots.plot!(equWgtsPerfs.data.timestamp, equWgtsPerfs.data.values*100, lab="Equal weights", line = (2, :red))
 
 ## get drawdowns
-ddowns = DynAssMgmt.evalDDowns(perfs)
+ddowns = evalDDowns(perfs)
 Plots.plot(ddowns, leg=:bottomright)
+Plots.gui()
 
 ## compute performance measures
 perfStats = evalPerfStats(perfs)
+perfSummary = DynAssMgmt.getPerfStatSummary(perfs)
 
-prices = convert(Prices, perfs)
-stdPrices = standardize(prices)
-
-nObs, nTimeSeries = size(prices.data.values)
-allPerfStats = Array(PerfStats, nTimeSeries)
-for ii=1:nTimeSeries
-    xxVals, xxNams = evalPerfStats(prices.data.values[:, ii])
-    thisPerfStats = DynAssMgmt.PerfStats(xxVals, xxNams)
-    allPerfStats[ii] = thisPerfStats
-end
-
-allPerfStats.FullPercRet
-
-allFields = fieldnames(allPerfStats[1])
-nMetrics = length(allFields)
-nStrats = length(allPerfStats)
-metricsSummary = zeros(Float64, nStrats, nMetrics)
-for ii=1:nStrats
-    thisResults = allPerfStats[ii]
-    for jj=1:nMetrics
-        metricsSummary[ii, jj] = getfield(thisResults, allFields[jj])
-    end
-end
-
-metricsNames = String[thisField for thisField in allFields]
-xx = NamedArray(metricsSummary, (stratLabels, metricsNames), ("Strategies", "Metrics"))
-
-convert(Array{Float64, 1}, xx[:, "FullPercRet"])
-
-
-Plots.plot(xx[:, "SigmaDailyToAnnualPerc"], xx[:, "MuDailyToAnnualPerc"], seriestype = :scatter)
-Plots.plot(xx[:, "MaxDD"], xx[:, "MuDailyToAnnualPerc"], seriestype = :scatter)
-
-
-
-using NamedArrays
-
-function getRealizedRiskReturn(prices::TimeSeries.TimeArray)
-
-    nStrats = size(prices.values, 2)
-    allPerfStats = []
-    for ii=1:nStrats
-        xxVals, xxNams = DynAssMgmt.evalPerfStats(prices.values[:, ii])
-        thisPerfStats = DynAssMgmt.PerfStats(xxVals, xxNams)
-        push!(allPerfStats, thisPerfStats)
-    end
-
-    # extract mus and sigmas
-    stratMus = [thisStrat.MuDailyToAnnualPerc for thisStrat in allPerfStats]
-    stratSigmas = [thisStrat.SigmaDailyToAnnualPerc for thisStrat in allPerfStats]
-
-    return stratMus, stratSigmas
-end
-
-xxInds = rets.data.timestamp .>= realizedPrices.data.timestamp[1]
+## compute performance measures for underlying assets
+xxInds = rets.data.timestamp .>= perfs.data.timestamp[1]
 btAssRets = Returns(rets.data[xxInds], rets.retType)
-assPrices = convert(Prices, btAssRets)
-assMus, assSigmas = getRealizedRiskReturn(assPrices.data)
-Plots.plot(assSigmas, assMus, seriestype = :scatter, label="Assets")
+assPerfs = convert(Performances, btAssRets)
+assPerfStats = DynAssMgmt.getPerfStatSummary(assPerfs)
 
-realizedPrices = convert(Prices, perfs)
-stratMus, stratSigmas = getRealizedRiskReturn(realizedPrices.data)
-Plots.plot!(stratSigmas, stratMus, seriestype = :scatter, label="Strategies")
-
-equWgtsPrices = convert(Prices, equWgtsPerfs)
-equWgtsMus, equWgtsSigmas = getRealizedRiskReturn(equWgtsPrices.data)
-Plots.plot!(equWgtsSigmas, equWgtsMus, seriestype = :scatter, label="Equal weights")
+## compute performance measures for equal weights
+equWgtsSummary = DynAssMgmt.getPerfStatSummary(equWgtsPerfs)
 
 ##
+Plots.plot(perfSummary[:, "MaxDD"], perfSummary[:, "MuDailyToAnnualPerc"], seriestype = :scatter,
+    xlabel="Maximum drawdown", ylabel="Annual percentage return", labels="Strategies")
+Plots.plot!(assPerfStats[:, "MaxDD"], assPerfStats[:, "MuDailyToAnnualPerc"], seriestype = :scatter,
+    labels="Assets")
+Plots.plot!(equWgtsSummary[:, "MaxDD"], equWgtsSummary[:, "MuDailyToAnnualPerc"], seriestype = :scatter,
+    labels="Equal weights")
+Plots.gui()
 
-prices = convert(Prices, perfs)
-xxVals, xxNams = DynAssMgmt.evalPerfStats(prices.data.values[:, 5])
-thisPerfStats = DynAssMgmt.PerfStats(xxVals, xxNams)
-
-
-# analyse weights
-# - TO
-# - diversification
-# - average
-
-using PyPlot
-using LaTeXStrings
-
-L"$y = \sin(x)$"
-
-
-using Plots
-dats = DynAssMgmt.getNumDates(logSynthPrices.timestamp)
-anim = Plots.@animate for ii=1:500:length(dats)
-    Plots.plot(dats[1:ii], logSynthPrices.values[1:ii, :], leg=false)
-end
-
-animGif = Plots.@gif for ii=1:100:length(dats)
-    Plots.plot(dats[1:ii], logSynthPrices.values[1:ii, :], leg=false)
-end
-
-gui(animGif)
-display(animGif)
+Plots.plot(perfSummary[:, "SigmaDailyToAnnualPerc"], perfSummary[:, "MuDailyToAnnualPerc"], seriestype = :scatter,
+    xlabel="Annual percentage vola", ylabel="Annual percentage return", labels="Strategies")
+Plots.plot!(assPerfStats[:, "SigmaDailyToAnnualPerc"], assPerfStats[:, "MuDailyToAnnualPerc"], seriestype = :scatter,
+    labels="Assets")
+Plots.plot!(equWgtsSummary[:, "SigmaDailyToAnnualPerc"], equWgtsSummary[:, "MuDailyToAnnualPerc"], seriestype = :scatter,
+    labels="Equal weights")
+Plots.gui()
